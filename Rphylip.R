b6d17e43bab635b7fb2461464986570c12f4e8b2
liamrevell <liamjrevell@gmail.com>
2013-12-14 13:03:18 -0500

    Rprotpars

--- Rphylip.R
+++ Rphylip.R
@@ -1,3 +1,95 @@
+## calls protpars from PHYLIP 3.695 (Felsenstein 2013)
+## written by Liam J. Revell 2013
+
+Rprotpars<-function(X,path=NULL,...){
+	if(is.null(path)) path<-findPath("protpars")
+	if(is.null(path)) stop("No path provided and was not able to find path to protpars")
+	if(class(X)!="proseq") stop("X should be an object of class 'proseq'")
+	if(hasArg(quiet)) quiet<-list(...)$quiet
+	else quiet<-FALSE
+	if(!quiet) if(file.warn(c("infile","intree","outfile","outtree","weights"))==0) return(NULL)
+	oo<-c("r")
+	if(hasArg(tree)){
+		oo<-c(oo,"u")
+		tree<-list(...)$tree
+		tree$tip.label<-sapply(tree$tip.label,function(x,y) which(x==y),y=rownames(X))
+		write.tree(tree,"intree")
+		intree<-TRUE
+	} else intree<-FALSE
+	if(hasArg(random.order)) random.order<-list(...)$random.order
+	else random.order<-TRUE
+	if(random.order){
+		if(hasArg(random.addition)) random.addition<-list(...)$random.addition
+		else random.addition<-10
+		oo<-c(oo,"j",sample(seq(1,99999,by=2),1),random.addition)
+	}
+	if(hasArg(threshold)) threshold<-list(...)$threshold
+	else threshold<-0
+	if(threshold!=0) oo<-c(oo,"t",threshold)
+	if(hasArg(genetic.code)){ 
+		genetic.code<-list(...)$genetic.code
+		oo<-c(oo,"c")
+		genetic.code<-tolower(genetic.code)
+		if(genetic.code=="universal") oo<-c(oo,"u")
+		else if(genetic.code=="mitochondrial") oo<-c(oo,"m")
+		else if(genetic.code=="vertebrate.mitochondrial") oo<-c(oo,"v")
+		else if(genetic.code=="fly.mitochondrial") oo<-c(oo,"f")
+		else if(genetic.code=="yeast.mitochondrial") oo<-c(oo,"y")
+		else {
+			cat(paste("Warning:\n  don't recognize genetic code of type",genetic.code,".\n"))
+			cat("   setting genetic code to type 'universal'.\n\n")
+			oo<-c(oo,"u")
+		}
+	}
+	if(hasArg(weights)){
+		oo<-c(oo,"w")
+		write(paste(weights,collapse=""),file="weights")
+	} else weights<-NULL
+	if(quiet) oo<-c(oo,2)
+	oo<-c(oo,"y","r")
+	write.dna(X)
+	system("touch outtree")
+	system("touch outfile")
+	system(paste(path,"/protpars",sep=""),input=oo)
+	tree<-read.tree("outtree")
+	temp<-readLines("outfile")
+	ii<-grep("requires a total of",temp)
+	for(i in 1:length(ii)){
+		xx<-strsplit(temp[ii[i]],"  ")[[1]]
+		if(length(ii)>1) tree[[i]]$pscore<-as.numeric(xx[length(xx)])
+		else tree$pscore<-as.numeric(xx[length(xx)])
+	}
+	temp<-lapply(temp,function(x) { cat(x); cat("\n") })
+	if(!quiet){
+		cat("Translation table\n")
+		cat("-----------------\n")
+		temp<-lapply(1:nrow(X),function(x,y) cat(paste("\t",paste(x,y[x],sep="\t"),"\n",sep="")),y=rownames(X))
+		cat("\n")
+	}
+	if(class(tree)=="phylo") tree$tip.label<-rownames(X)[as.numeric(tree$tip.label)]
+	else if(class(tree)=="multiPhylo"){
+		foo<-function(x,y){
+			x$tip.label<-y[as.numeric(x$tip.label)]
+			x
+		}
+		tree<-lapply(tree,foo,y=rownames(X))
+		class(tree)<-"multiPhylo"
+	}	
+	if(hasArg(outgroup)){ 
+		outgroup<-list(...)$outgroup
+		tree<-outgroup.root(tree,outgroup,quiet)
+	}
+	if(hasArg(cleanup)) cleanup<-list(...)$cleanup
+	else cleanup<-TRUE
+	if(cleanup){
+		files<-c("infile","outfile","outtree")
+		if(!is.null(weights)) files<-c(files,"weights")
+		if(intree) files<-c(files,"intree")
+		cleanFiles(files)
+	}
+	return(tree)
+}
+
 ## convert phangorn phyDat to proseq
 ## written by Liam J. Revell 2013
 
@@ -827,7 +919,7 @@ Rdnapars<-function(X,path=NULL,...){
 	if(class(X)!="DNAbin") stop("X should be an object of class 'DNAbin'")
 	if(hasArg(quiet)) quiet<-list(...)$quiet
 	else quiet<-FALSE
-	if(!quiet) if(file.warn(c("infile","outfile","outtree","weights"))==0) return(NULL)
+	if(!quiet) if(file.warn(c("infile","intree","outfile","outtree","weights"))==0) return(NULL)
 	oo<-c("r")
 	if(hasArg(tree)){
 		oo<-c(oo,"u")
