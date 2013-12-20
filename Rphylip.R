2157fab66960534a524a2d13995afd2df7f066aa
liamrevell <liamjrevell@gmail.com>
2013-12-20 00:20:59 -0500

    New fn Rfitch
    Rfitch does LS & ME phylogeny inference using FITCH

--- Rphylip.R
+++ Rphylip.R
@@ -1,3 +1,79 @@
+## calls fitch from PHYLIP 3.695 (Felsenstein 2013)
+## written by Liam J. Revell 2013
+
+Rfitch<-function(D,path=NULL,...){
+	if(class(D)=="dist"||class(D)=="data.frame") D<-as.matrix(D)
+	D<-D[rownames(D),rownames(D)]
+	if(is.null(path)) path<-findPath("fitch")
+	if(is.null(path)) stop("No path provided and was not able to find path to fitch")
+	if(hasArg(quiet)) quiet<-list(...)$quiet
+	else quiet<-FALSE
+	if(!quiet) if(file.warn(c("infile","intree","outfile","outtree"))==0) return(NULL)
+	oo<-c("r")
+	if(hasArg(method)) method<-list(...)$method
+	else method<-"fm"
+	if(method=="FM"||method=="fm") method<-"fm"
+	else if(method=="ME"||method=="me"){
+		method<-"me"
+		oo<-c(oo,"d")
+	} else {
+		cat("\nWarning:\n  method not recognized - using method=\"FM\"\n")
+		method="fm"
+	}
+	if(hasArg(tree)){
+		oo<-c(oo,"u")
+		tree<-list(...)$tree
+		tree$tip.label<-sapply(tree$tip.label,function(x,y) which(x==y),y=rownames(D))
+		write.tree(tree,"intree")
+		intree<-TRUE
+	} else intree<-FALSE
+	if(hasArg(power)){
+		power<-list(...)$power
+		oo<-c(oo,"p",power)
+	}
+	if(hasArg(negative)) negative<-list(...)$negative
+	else negative<-TRUE
+	if(!negative) oo<-c(oo,"-")
+	if(hasArg(global)) global<-list(...)$global
+	else global<-TRUE
+	if(global) oo<-c(oo,"g")
+	if(hasArg(random.order)) random.order<-list(...)$random.order
+	else random.order<-TRUE
+	if(random.order){
+		if(hasArg(random.addition)) random.addition<-list(...)$random.addition
+		else random.addition<-10
+		oo<-c(oo,"j",sample(seq(1,99999,by=2),1),random.addition)
+	}
+	if(quiet) oo<-c(oo,2)
+	oo<-c(oo,"y","r")
+	write.distances(D)
+	system("touch outtree")
+	system("touch outfile")
+	system(paste(path,"/fitch",sep=""),input=oo)
+	tree<-read.tree("outtree")
+	temp<-readLines("outfile")
+	temp<-lapply(temp,function(x) { cat(x); cat("\n") })
+	if(!quiet){
+		cat("Translation table\n")
+		cat("-----------------\n")
+		temp<-lapply(1:nrow(D),function(x,y) cat(paste("\t",paste(x,y[x],sep="\t"),"\n",sep="")),y=rownames(D))
+		cat("\n")
+	}
+	tree$tip.label<-rownames(D)[as.numeric(tree$tip.label)]	
+	if(hasArg(outgroup)){
+		outgroup<-list(...)$outgroup
+		tree<-outgroup.root(tree,outgroup,quiet)
+	}
+	if(hasArg(cleanup)) cleanup<-list(...)$cleanup
+	else cleanup<-TRUE
+	if(cleanup){
+		files<-c("infile","outfile","outtree")
+		if(intree) files<-c(files,"intree")
+		cleanFiles(files)
+	}
+	return(tree)
+}
+
 ## call dnainvar from PHYLIP 3.695 (Felsenstein 2013)
 ## written by Liam J. Revell 2013
 
